e5682c8951d0e30d66fed2081e35d89bb66a1026
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-07-30 12:26:21 -0700

    mongoose errors

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,20 +1,8 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks-app');
-
-// module.exports = mongoose.connection;
-
-
 const mongoose = require('mongoose');
 
-// Replace 'googlebooks-app' with your actual database name
-const uri = 'mongodb://127.0.0.1:27017/googlebooks-app';
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks-app');
 
 module.exports = mongoose.connection;
+
+
+
